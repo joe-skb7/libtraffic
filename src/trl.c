3c053f56ae34cf82b86248565548d82c113a2947
Sam Protsenko <semen.protsenko@linaro.org>
2018-11-20 17:23:55 +0200

    Add testing code
    Tested: it works.
    
    Signed-off-by: Sam Protsenko <semen.protsenko@linaro.org>

--- src/trl.c
+++ src/trl.c
@@ -11,19 +11,24 @@
 
 #define LIB_NAME	"libtrl"
 
-#define PIN_TX		0x01  /* Orange wire on FTDI cable */
-#define PIX_RX		0x02  /* Yellow */
-#define PIN_RTS		0x04  /* Green */
-#define PIN_CTS		0x08  /* Brown */
+/* USB "Vendor ID" and "Product ID" for FTDI chip */
+#define FTDI_VID	0x0403
+#define FTDI_PID	0x6001
+
+#define PIN_TXD		0x01  /* Orange (on FTDI cable) */
+#define PIN_RXD		0x02  /* Yellow (on FTDI cable) */
+#define PIN_RTS		0x04  /* Green  (on FTDI cable) */
+#define PIN_CTS		0x08  /* Brown  (on FTDI cable) */
 #define PIN_DTR		0x10
 #define PIN_DSR		0x20
 #define PIN_DCD		0x40
 #define PIN_RI		0x80
 
-#define FTDI_VID	0x0403
-#define FTDI_PID	0x6001
-#define LED		PIN_CTS
-#define DELAY		1000	/* msec */
+#define GPIO0		PIN_CTS
+#define GPIO1		PIN_TXD
+#define GPIO2		PIN_RXD
+/* On SparkFun breakout board we have DTR in place of RTS, so let's use both */
+#define GPIO3		(PIN_RTS | PIN_DTR)
 
 #define TRL_LOG(f, fmt, ...)						\
 	do {								\
@@ -42,29 +47,26 @@ int trl_init(void)
 {
 	int res, ret;
 
-	if (trl.ftdi != NULL) {
-		TRL_LOG(stderr, "Error: trl_init() was already invoked\n");
-		return -1;
-	}
+	assert(trl.ftdi == NULL);
 
 	trl.ftdi = ftdi_new();
 	if (trl.ftdi == NULL) {
 		TRL_LOG(stderr, "Error: Can't allocate memory for FTDI obj\n");
-		return -2;
+		return -1;
 	}
 
 	res = ftdi_usb_open(trl.ftdi, FTDI_VID, FTDI_PID);
 	if (res < 0) {
 		TRL_LOG(stderr, "Error: Unable to open FTDI device: %d (%s)\n",
 				res, ftdi_get_error_string(trl.ftdi));
-		ret = -3;
+		ret = -2;
 		goto err1;
 	}
 
 	res = ftdi_set_bitmode(trl.ftdi, 0xff, BITMODE_BITBANG);
 	if (res < 0) {
 		TRL_LOG(stderr, "Error: Can't enable bitbang\n");
-		ret = -4;
+		ret = -3;
 		goto err2;
 	}
 
@@ -80,48 +82,67 @@ err1:
 /* TODO: Implement this one */
 int trl_set_one(int num, int state)
 {
+	assert(trl.ftdi != NULL);
+
 	/* XXX: Remove */
 	TRL_LOG(stdout, "stub\n");
-	(void)num;
-	(void)state;
+	(void)num;	/* Unused */
+	(void)state;	/* Unused */
 	return 0;
 }
 
 /* TODO: Implement this one */
 int trl_set_burst(uint16_t mask)
 {
-	/* XXX: Remove */
-	TRL_LOG(stdout, "stub\n");
-	(void)mask;
+	int res;
+	int i;
+	unsigned char data = 0;
+
+	assert(trl.ftdi != NULL);
+
+	(void)mask; /* Unused */
+
+	for (i = 0; i < 5; ++i) {
+		int j;
+
+		for (j = 0; j < 4; ++j) {
+			switch (j) {
+			case 0:
+				data = GPIO0;
+				break;
+			case 1:
+				data = GPIO1;
+				break;
+			case 2:
+				data = GPIO2;
+				break;
+			case 3:
+				data = GPIO3;
+				break;
+			}
+
+			res = ftdi_write_data(trl.ftdi, &data, 1);
+			if (res < 0) {
+				TRL_LOG(stderr,
+					"Error: Write failed for 0x%x: %s\n",
+					data, ftdi_get_error_string(trl.ftdi));
+				return -1;
+			}
+
+			msleep(125);
+		}
+	}
+
 	return 0;
 }
 
 void trl_exit(void)
 {
+	assert(trl.ftdi != NULL);
+
 	ftdi_disable_bitbang(trl.ftdi);
 	ftdi_usb_close(trl.ftdi);
 	ftdi_free(trl.ftdi);
+
 	memset(&trl, 0, sizeof(struct trl));
 }
-
-#if 0
-int main(void)
-{
-	struct ftdi_context *ftdi;
-	int res, ret;
-	int i;
-	unsigned char data = 0;
-
-	for (i = 0; i < 10; ++i) {
-		data ^= LED;
-		res = ftdi_write_data(ftdi, &data, 1);
-		if (res < 0) {
-			TRL_LOG(stderr, "Error: Write failed for 0x%x: %s\n",
-					data, ftdi_get_error_string(ftdi));
-			ret = EXIT_FAILURE;
-			goto err3;
-		}
-		msleep(DELAY);
-	}
-}
-#endif
