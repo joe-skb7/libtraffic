683ac03a229fe99a0c6338446c5e7d8b2cc018d3
Sam Protsenko <semen.protsenko@linaro.org>
2018-11-20 02:42:16 +0200

    Improve example.
    Signed-off-by: Sam Protsenko <semen.protsenko@linaro.org>

--- example/main.c
+++ example/main.c
@@ -1,3 +1,7 @@
+/**
+ * Tiny app that demonstrates working with libtrl (Traffic Light Library).
+ */
+
 #include <trl.h>
 
 #include <stdio.h>
@@ -8,26 +12,33 @@ int main(void)
 	int res;
 	int ret = EXIT_SUCCESS;
 
+	/* Power-on libtrl */
 	res = trl_init();
 	if (res < 0)
 		return EXIT_FAILURE;
 
+	/* Set all traffic lights to "green" state */
 	res = trl_set_burst(0xffff);
 	if (res < 0) {
 		ret = EXIT_FAILURE;
 		goto err;
 	}
 
-	res = trl_set_one(0, TRL_STATE_GREEN);
+	/* Set traffic light #0 to "red" state */
+	res = trl_set_one(0, TRL_STATE_RED);
 	if (res < 0) {
 		ret = EXIT_FAILURE;
 		goto err;
 	}
 
+	/* Power-off libtrl */
 	trl_exit();
 
-	printf("Success!\n");
-
 err:
+	if (ret == EXIT_SUCCESS)
+		printf("Success!\n");
+	else
+		fprintf(stderr, "Failure!\n");
+
 	return ret;
 }
