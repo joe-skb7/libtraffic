bb87f5701a8ebb7bfd86cfacca0269da9e0f783f
Sam Protsenko <semen.protsenko@linaro.org>
2018-12-07 20:32:05 +0200

    Improve example (make sanity test of it)
    Signed-off-by: Sam Protsenko <semen.protsenko@linaro.org>

--- example/main.c
+++ example/main.c
@@ -2,43 +2,89 @@
  * Tiny app that demonstrates working with libtrl (Traffic Light Library).
  */
 
+#include <tools.h>
+
 #include <trl.h>
 
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#define EXIT_SIGNAL	128	/* terminated by signal */
+
+static void sig_handler(int signum)
+{
+	if (signum == SIGINT) {
+		trl_set_burst(0x0000); /* set all to red */
+		trl_exit();
+		exit(EXIT_SIGNAL + signum);
+	}
+}
 
 int main(void)
 {
 	int res;
-	int ret = EXIT_SUCCESS;
+
+	if (signal(SIGINT, sig_handler) == SIG_ERR)
+		fprintf(stderr, "Warning: Can't catch SIGINT\n");
 
 	/* Power-on libtrl */
 	res = trl_init();
 	if (res < 0)
 		return EXIT_FAILURE;
 
-	/* Set all traffic lights to "green" state */
-	res = trl_set_burst(0xffff);
-	if (res < 0) {
-		ret = EXIT_FAILURE;
-		goto err;
+	/* Sanity test traffic lights */
+	for (;;) {
+		int j;
+
+		/* Set all traffic lights to "green" state */
+		res = trl_set_burst(0xffff);
+		if (res < 0)
+			goto err;
+		msleep(500);
+
+
+		/* Set all traffic lights to "red" state */
+		res = trl_set_burst(0x0000);
+		if (res < 0)
+			goto err;
+		msleep(500);
+
+		/* Set all traffic lights to pattern #1 */
+		res = trl_set_burst(0xaaaa);
+		if (res < 0)
+			goto err;
+		msleep(500);
+
+		/* Set all traffic lights to pattern #2 */
+		res = trl_set_burst(0x5555);
+		if (res < 0)
+			goto err;
+		msleep(500);
+
+		/* Set all traffic lights to "red" state */
+		res = trl_set_burst(0x0000);
+		if (res < 0)
+			goto err;
+
+		for (j = 0; j < 10; j++) {
+			res = trl_set_one(j, TRL_STATE_GREEN);
+			if (res < 0)
+				goto err;
+			msleep(100);
+		}
+
+		for (j = 9; j >= 0; j--) {
+			res = trl_set_one(j, TRL_STATE_RED);
+			if (res < 0)
+				goto err;
+			msleep(100);
+		}
 	}
 
-	/* Set traffic light #0 to "red" state */
-	res = trl_set_one(0, TRL_STATE_RED);
-	if (res < 0) {
-		ret = EXIT_FAILURE;
-		goto err;
-	}
-
-	/* Power-off libtrl */
-	trl_exit();
+	/* Does not return; exit happens in Ctrl-C handler */
 
 err:
-	if (ret == EXIT_SUCCESS)
-		printf("Success!\n");
-	else
-		fprintf(stderr, "Failure!\n");
-
-	return ret;
+	fprintf(stderr, "Failure!\n");
+	return EXIT_FAILURE;
 }
